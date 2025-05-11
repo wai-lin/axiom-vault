a0ad438f989dfe2c63d8b4e944acdb39352bd5b0
TomEverson <studycafe.125@gmail.com>
2025-05-11 18:46:58 +0700

    Update: Mempool Basic Design

--- final_project/messages.py
+++ final_project/messages.py
@@ -1,8 +1,23 @@
 from ipv8.messaging.payload_dataclass import dataclass
+import hashlib
+import json
+
 
 @dataclass(msg_id=1)
-class BetTicketMessage:
+class BetPayload:
     bettor_id: str
     bet_number: int
-    bet_amount: float
-    timestamp: str
+    bet_amount: int
+    timestamp: float
+    signature: str
+
+    def _generate_txid(self):
+        payload_data = {
+            "bettor_id": self.bettor_id,
+            "bet_number": self.bet_number,
+            "bet_amount": self.bet_amount,
+            "timestamp": self.timestamp,
+        }
+        payload_bytes = json.dumps(
+            payload_data, sort_keys=True).encode('utf-8')
+        return hashlib.sha256(payload_bytes).hexdigest()
